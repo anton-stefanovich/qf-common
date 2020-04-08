86bd764f69478d37c8129a95028f74dd0faf40cf
Anton Stefanovich <anton.stefanovich@pwc.com>
2020-04-08 18:17:44 -0400

    Package renamed to 'qf_common' for simple import usage
    .gitignore updated with some extra file patterns

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='qf-common',
-    version='0.1',
+    version='0.4',
     author='Anton Stefanovich',
     description='Useful things for QF projects',
     url='https://github.com/quality-first/qf-common',
     author_email='anton.stefanovich@gmail.com',
-    packages=['src'],
+    packages=['qf_common'],
     zip_safe=False,
     license='MIT',
 )
