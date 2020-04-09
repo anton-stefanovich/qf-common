a107ab659e8c14fd11bbfb685fc0156fb289d787
Anton Stefanovich <anton.stefanovich@pwc.com>
2020-04-09 13:31:23 -0400

    TestInput renamed to __TestInput to disable pytest errors

--- tests/data/test_any_to_bool.py
+++ tests/data/test_any_to_bool.py
@@ -2,7 +2,7 @@ from qf_common.test import pytest_parametrize
 from enum import Enum
 
 
-class TestInput (Enum):
+class __TestInput (Enum):
     STRING_JUNK = ['10', '01', '0x', 'Junk', None]
     STRING_TRUE = ['Yes', 'True', 'Y', 'On', '1', 'Enable', 'Enabled']
     STRING_FALSE = ['No', 'False', 'N', 'Off', '0', 'Disable', 'Disabled']
@@ -24,56 +24,56 @@ def __check_bool_conversion(*args, reference_value: bool = None, **kwargs):
                result == reference_value
 
 
-@pytest_parametrize(*TestInput.STRING_TRUE.value)
+@pytest_parametrize(*__TestInput.STRING_TRUE.value)
 def test__raw_true_string_to_bool__no_default_value(variant):
     __check_bool_conversion(variant, reference_value=True)
 
 
-@pytest_parametrize(*TestInput.STRING_TRUE.value)
+@pytest_parametrize(*__TestInput.STRING_TRUE.value)
 def test__raw_true_string_to_bool__false_as_default_value(variant):
     __check_bool_conversion(variant, default=False, reference_value=True)
 
 
-@pytest_parametrize(*(string.upper() for string in TestInput.STRING_TRUE.value))
+@pytest_parametrize(*(string.upper() for string in __TestInput.STRING_TRUE.value))
 def test__uppercase_true_string_to_bool__no_default_value(variant):
     __check_bool_conversion(variant, reference_value=True)
 
 
-@pytest_parametrize(*(string.lower() for string in TestInput.STRING_TRUE.value))
+@pytest_parametrize(*(string.lower() for string in __TestInput.STRING_TRUE.value))
 def test__lowercase_true_string_to_bool__no_default_value(variant):
     __check_bool_conversion(variant, reference_value=True)
 
 
-@pytest_parametrize(*TestInput.STRING_FALSE.value)
+@pytest_parametrize(*__TestInput.STRING_FALSE.value)
 def test__raw_false_string_to_bool__no_default_value(variant):
     __check_bool_conversion(variant, reference_value=False)
 
 
-@pytest_parametrize(*TestInput.STRING_FALSE.value)
+@pytest_parametrize(*__TestInput.STRING_FALSE.value)
 def test__raw_false_string_to_bool__true_as_default_value(variant):
     __check_bool_conversion(variant, default=True, reference_value=False)
 
 
-@pytest_parametrize(*(string.upper() for string in TestInput.STRING_FALSE.value))
+@pytest_parametrize(*(string.upper() for string in __TestInput.STRING_FALSE.value))
 def test__uppercase_false_string_to_bool__no_default_value(variant):
     __check_bool_conversion(variant, reference_value=False)
 
 
-@pytest_parametrize(*(string.lower() for string in TestInput.STRING_FALSE.value))
+@pytest_parametrize(*(string.lower() for string in __TestInput.STRING_FALSE.value))
 def test__lowercase_false_string_to_bool__no_default_value(variant):
     __check_bool_conversion(variant, reference_value=False)
 
 
-@pytest_parametrize(*TestInput.STRING_JUNK.value)
+@pytest_parametrize(*__TestInput.STRING_JUNK.value)
 def test__junk_string_to_bool__no_default_value(variant):
     __check_bool_conversion(variant)
 
 
-@pytest_parametrize(*TestInput.STRING_JUNK.value)
+@pytest_parametrize(*__TestInput.STRING_JUNK.value)
 def test__junk_string_to_bool__true_as_default_value(variant):
     __check_bool_conversion(variant, default=True, reference_value=True)
 
 
-@pytest_parametrize(*TestInput.STRING_JUNK.value)
+@pytest_parametrize(*__TestInput.STRING_JUNK.value)
 def test__junk_string_to_bool__false_as_default_value(variant):
     __check_bool_conversion(variant, default=False, reference_value=False)
