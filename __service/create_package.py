413776baa34e5140c69cd5f00c59a87dff3ea102
Anton Stefanovich <anton.stefanovich@pwc.com>
2020-04-10 20:52:10 -0400

    YAML script updated with the set of jobs
    - New package creation and publishing separated by different scripts

---
+++ __service/create_package.py
@@ -0,0 +1,7 @@
+from qf_common import package
+
+package.create(
+    name='qf-common', packages=['qf_common'],
+    description='Useful things for QF projects',
+    url='https://github.com/quality-first/qf-common',
+)
--- __service/share_package.py
+++ __service/share_package.py
@@ -5,13 +5,10 @@ from qf_common import package
 parser = ArgumentParser()
 parser.add_argument('--username', '-u', type=str, required=True)
 parser.add_argument('--password', '-p', type=str, required=True)
+parser.add_argument('--files', '-f', type=list, required=True)
 params = parser.parse_args()
 
 package.share(
     params.username,
     params.password,
-    package.create(
-        name='qf-common', packages=['qf_common'],
-        description='Useful things for QF projects',
-        url='https://github.com/quality-first/qf-common',
-    ))
+    params.files)
